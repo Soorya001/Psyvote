855bdf4868e0a10a003ef357a167731d4e085a3c
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-04-14 18:35:45 +0530

    Created Tables and added starter for backend

---
+++ server/index.js
@@ -0,0 +1,25 @@
+//importing
+import express from "express";
+import cors from "cors";
+import { GetData } from "./routes/dbdata.js";
+
+//app config
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+//DB configs
+
+//api routes
+app.get("/", GetData); //demo for data from oracle -> date from dual table
+
+//listen
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
